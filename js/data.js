920235cc25d7ed3ac201b5711e56c773295d1fd7
Alexander Khant <grashupfer99@gmail.com>
2018-05-12 14:59:56 +0900

    initial commit

---
+++ js/data.js
@@ -0,0 +1,107 @@
+/* data.js to store arrays of quotes and colors */
+
+// Array of custom color codes
+const colors = [
+    '5356E3',   // Royal Blue
+    'D54C50',   // Valencia
+    '3A1C91',   // Daisy Bush
+    '1892D3',   // Curious Blue
+    '661120',   // Dark Tan
+    '9A7854',   // Leather
+    '2C9210',   // La Palma
+    '4F2642',   // Livid Brown
+    '4C4E22',   // Woodland
+    '11183D',   // Bunting
+    'A11CE6',   // Electric Violet
+    '86338D',   // Vivid Violet
+    '969033',   // Sycamore
+    '266E53',   // Green Pea
+    'FA0B13',   // Red
+    '710202',   // Lonestar
+    'DB0528'    // Monza
+];
+
+// An array of JavaScript objects that holds the data for all[15] quotes.
+const quotes = [
+    {
+        quote: 'Learning never exhausts the mind.',
+        source: 'Leonardo da Vinci',
+        tags: 'learning'
+    },
+    {
+        quote: 'The empires of the future are the empires of the mind.',
+        source: 'Winston Churchull',
+        year: '6 September 1943',
+        tags: 'politics'
+    },
+    {
+        quote: 'Friendship ... is born at the moment when one man says to another "What! You too? I thought that no one but myself...',
+        source: 'C.S.Lewis',
+        citation: 'The Four Loves',
+        tags: 'literature'
+    },
+    {
+        quote: 'There is some good in this world, and it’s worth fighting for.',
+        source: 'J.R.R. Tolkien',
+        citation: 'The Two Towers',
+        tags: 'literature'
+    },
+    {
+        quote: 'An inverstement in knowledge pays the best interest.',
+        source: 'Benjamin Franklin',
+        tags: 'education'
+    },
+    {
+        quote: 'Knowledge is of no value unless you put it into practice.',
+        source: 'Anton Chekhov',
+        tags: 'practice value'
+    },
+    {
+        quote: 'Great things in business are never done by one person. They\'re done by a team of people.',
+        source: 'Steve Jobs',
+        tags: 'business'
+    },
+    {
+        quote: 'No man ever steps in the same river twice, for it\'s not the same river and he\'s not the same man',
+        source: 'Heraclitus',
+        year: '500 BC',
+        tags: 'philosophy'
+    },
+    {
+        quote: 'And whatsoever ye do, do it heartily, as to the Lord, and not unto men',
+        source: 'Paul the Apostle',
+        citation: 'The Holy Bible',
+        tags: 'christianity'
+    },
+    {
+        quote: 'The past cannot be changed. The future is yet in your power.',
+        source: 'Unknown',
+        tags: 'motivational past'
+    },
+    {
+        quote: 'A new command I give you: Love one another. As I have loved you, so you must love one another.',
+        source: 'Jesus Christ',
+        citation: 'The Holy Bible',
+        tags: 'trust god'
+    },
+    {
+        quote: 'You miss 100% of the shots you don\'t take.',
+        source: 'Wayne Gretzky',
+        tags: 'miss shots take'
+    },
+    {
+        quote: 'The journey of a thousand miles begins in one step.',
+        source: 'Lao Tzu',
+        tags: 'wisdom'
+    },
+    {
+        quote: 'If you have no critics you\'ll likely have no success.',
+        source: 'Malcolm X',
+        tags: 'critics success'
+    },
+    {
+        quote: 'If you fell down yesterday, stand up today.',
+        source: 'H.G.Wells',
+        tags: 'motivational'
+    },
+];
---
+++ js/script.js
@@ -0,0 +1,88 @@
+/* script.js contains main functions for the Random Quote Generator project */
+
+/*
+* Project name:         Random Quote Generator
+* Student name:         Alex Khant (https://github.com/grashupfer99)
+* Updated:              2018-05-12
+*/
+
+// Setting up variables for our timer
+let timer;
+let delay = 20000;
+
+// Storing DOM strings in one place in case they are changed
+const DOMstrings = {
+    body: 'body',
+    loadQuote: 'loadQuote', 
+    quoteBox: 'quote-box'
+}
+
+// Selecting a random quote from the array of quotes and returning a randomly selected quote object.
+function getRandomQuote() {
+    const rand = Math.floor(Math.random() * quotes.length);
+    console.log(rand);
+    return quotes[rand];
+}
+
+// Printing out the randomly selected quote
+function printQuote() {
+
+    const randQuote = getRandomQuote();
+
+    // Building an HTML template using a string
+    let html = '';
+    html += '<p class="quote">' + randQuote.quote + '</p>';
+    html += '<p class="source">' + randQuote.source;
+    
+    // Add a citation if it's found in the array object
+    if(randQuote.hasOwnProperty('citation')){
+        html += '<span class="citation">' + randQuote.citation + '</span>';
+    } 
+
+    // Add a year if it's found in the array object
+    if (randQuote.hasOwnProperty('year')){
+        html += '<span class="year">' + randQuote.year + '</span>';
+    }
+
+    if(randQuote.hasOwnProperty('tags')){
+        // Handling cases with two or more tags
+        let tag = randQuote.tags.split(' ').join(' #');
+        html += '<span class="tags">' + '#' + tag + '</span>';
+    }
+    html += '</p>';
+
+    // Selecting the body tag and changing its background color  
+    document.getElementsByTagName(DOMstrings.body)[0].style.backgroundColor = randColorGenerator();
+
+    // Returning the result
+    return document.getElementById(DOMstrings.quoteBox).innerHTML = html;
+}
+
+// Random hex color generator
+function randColorGenerator() {
+    let hexColor = "#";
+    let color = [Math.floor(Math.random() * colors.length)];
+    hexColor += colors[color];
+    return hexColor;
+}
+
+// Updating the timer
+function updateTimer(){
+    clearInterval(timer);
+    timer = setInterval(printQuote, delay);
+}
+
+// Invoking the printQuote function
+printQuote(); 
+// Setting up a timer to display quotes every delay(the value is stored in delay variable) ms. 
+timer = setInterval(printQuote, delay);
+
+// event listener to respond to "Show another quote" button clicks
+// when user clicks anywhere on the button, the "printQuote" function is called
+document.getElementById(DOMstrings.loadQuote).addEventListener("click", function () {
+    // Implementing two actions: printing out a new quote and updating the timer  
+    printQuote();
+    updateTimer();
+}, false);
+
+
